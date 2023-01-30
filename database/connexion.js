53bc98213744474ade3b9b9ad2a7f0913e331c91
Romain kabasi <kabasiromain@gmail.com>
2023-01-30 14:16:36 +0100

    feat: signin and signup users

--- database/connexion.js
+++ database/connexion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
 
-    mongoose.connect("mongodb+srv://romainkabasi:muhika_13@cluster0.uw2zabe.mongodb.net/?retryWrites=true&w=majority",
+    mongoose.connect("mongodb+srv://romainkabasi:muhika_13@cluster0.pj1idih.mongodb.net/?retryWrites=true&w=majority",
         {
             useNewUrlParser:true,
             useUnifiedTopology:true
